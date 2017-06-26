80fead8a15c4d0ed0621c267a05c4ff6253c97c6
youngsing <youngsingyx@gmail.com>
2017-06-26 16:50:19 +0800

    support webhook

--- plugins/etm_webhook.py
+++ plugins/etm_webhook.py
@@ -1,10 +1,23 @@
+"""
+extend with webhook
+"""
+# coding=utf-8
 from plugins.eh_telegram_master import TelegramChannel
 import config
 
 class ETM_Webhook(TelegramChannel):
     def polling_from_tg(self):
-        webhook_url = ''
-        token = getattr(config, self.channel_id)['token']
-        self.bot.start_webhook(listen='0.0.0.0', port=5002, url_path=token)
-        webhook_url += token
-        self.bot.bot.setWebhook(webhook_url=webhook_url)
+        """
+        copy from
+        https://github.com/RoyXiang/ehForwarderBot/commit/dc43f8106535d3f6576ab6597864a12a9b7bdc1b
+        """
+        webhook_url = getattr(config, 'webhook_url', default='')
+        if webhook_url != '':
+            token = getattr(config, self.channel_id)['token']
+            self.bot.start_webhook(listen='0.0.0.0', port=5002, url_path=token)
+            if not webhook_url.endswith('/'):
+                webhook_url += '/'
+            webhook_url += token
+            self.bot.bot.setWebhook(webhook_url=webhook_url)
+        else:
+            self.bot.start_polling(timeout=10)
